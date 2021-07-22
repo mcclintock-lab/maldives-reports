8a9f995017a6da4376f0a52888b14ca3aeaf44d2
Tim Welch <tim.j.welch@gmail.com>
2021-07-22 09:28:27 -0700

    missed raster

--- src/functions/habitatRaster.ts
+++ src/functions/habitatRaster.ts
@@ -4,20 +4,26 @@ import {
   Sketch,
   SketchCollection,
   isFeatureCollection,
+  roundDecimal,
 } from "@seasketch/geoprocessing";
 import bbox from "@turf/bbox";
-import { config } from "./habitatConfig";
+import { config, HabitatResults } from "./habitatConfig";
 import { areaByClassRaster } from "../util/areaByClass";
 import logger from "../util/logger";
 import dissolve from "@turf/dissolve";
 import { featureCollection } from "@turf/helpers";
+
+// Must be generated first by habitat-4-precalc
+// TODO: migrate to habitatAreaStatsRaster.json
+// Note: habitat-4-precalc-raster does not generate correct totals.  Fix or augment with the qgis precalc numbers in habitatConfig.ts
+import habitatAreaStats from "../../data/precalc/habitatAreaStats.json";
 
 /**
  * Returns the area captured by the Feature polygon(s) using rasters
  */
 export async function habitatRaster(
   sketch: Sketch<Polygon> | SketchCollection<Polygon>
-) {
+): Promise<HabitatResults> {
   if (!sketch) throw new Error("Feature is missing");
 
   try {
@@ -27,7 +33,16 @@ export async function habitatRaster(
       : featureCollection([sketch]);
 
     const box = sketch.bbox || bbox(sketch);
-    return areaByClassRaster(fc, box, config);
+    const areaByClass = await areaByClassRaster(fc, box, config);
+
+    return {
+      ...habitatAreaStats,
+      success: true,
+      areaByClass: habitatAreaStats.areaByClass.map((abc) => ({
+        ...abc,
+        sketchArea: roundDecimal(areaByClass[abc.class_id] || 0, 6),
+      })),
+    };
   } catch (err) {
     logger.error("habitat error", err);
     throw err;
