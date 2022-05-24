fd8afdc6d2b3060c59e8519c7ecee0b833144237
Tim Welch <tim.j.welch@gmail.com>
2022-05-24 14:23:00 -0700

    all data rep

---
+++ data/all-data-prep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run outside workspace
+
+./basin-geomorphic-1-data-prep.sh
+./benthic-species-1-data-prep.sh
+./boundary-1-data-prep.sh
+./depth-1-data-prep.sh
+./enviroZone-1-data-prep.sh
+./oceanuse-1-data-prep.sh
+./single-geomorphic-1-data-prep.sh
