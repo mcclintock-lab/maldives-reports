e127743b2f9deb25723432037edccc9f042e47d8
Tim Welch <tim.j.welch@gmail.com>
2022-08-23 21:34:49 -0700

    bump memory

--- src/functions/ousValueOverlap.ts
+++ src/functions/ousValueOverlap.ts
@@ -63,5 +63,5 @@ export default new GeoprocessingHandler(ousValueOverlap, {
   executionMode: "async",
   // Specify any Sketch Class form attributes that are required
   requiresProperties: [],
-  memory: 8192,
+  memory: 10240,
 });
